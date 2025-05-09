b7a6b6a51cae259cc6839f3d8cc6201186b8c03c
Peter Ritter <peter@cygnus.systems>
2025-05-09 17:08:43 +0100

    chore: comment out unused imports in test_utils.py

--- testing/test_utils.py
+++ testing/test_utils.py
@@ -1,7 +1,7 @@
 # This file is kept for backward compatibility
 # Import everything from conftest.py
-from conftest import (
-    SSH_HOST, SSH_PORT, SSH_USER, SSH_PASSWORD, SSH_KEYFILE, 
-    TEST_SUDO_PASSWORD, get_client, cleanup_client, 
-    print_test_header, print_test_footer
-)
+# from conftest import (
+#     SSH_HOST, SSH_PORT, SSH_USER, SSH_PASSWORD, SSH_KEYFILE,
+#     TEST_SUDO_PASSWORD, get_client, cleanup_client,
+#     print_test_header, print_test_footer
+# )
